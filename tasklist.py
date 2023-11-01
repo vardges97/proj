7f5f8a786cce42b259d42e9af14aa7161e5a2cf4
vardges <107487055+vardges97@users.noreply.github.com>
2023-11-01 17:10:53 +0400

    Add files via upload

---
+++ tasklist.py
@@ -0,0 +1,101 @@
+task_list = []
+
+def generate_id():
+    if not task_list:
+        return 1
+    return max(task['id'] for task in task_list) + 1
+
+def add_task():
+    description = input('enter the name of the task: ')
+    if not description:
+        print('task description cant be empty')
+        return
+    priority = ('')
+    priority == input("enter a priority for the task( low, medium, high): ").lower
+    if priority not in ('low,medium,high'):
+        print('invalid task must have priority')
+        return
+    task = {
+        'id':generate_id(),
+        'description': description,
+        'compeletion': None,
+        'priority': priority
+    }
+    task_list.append(task)
+    print('task added successfully')
+
+def remove_task():
+    task_id = int(input('please enter the task id which you want removed:'))
+    for task in task_list:
+        task_id == task['id']
+        task_list.remove(task)
+        print('task removed')
+        return
+    else:
+        print('task not found in task list')
+
+def list_tasks():
+    if not task_list:
+        print('no such task found')
+        return
+    for task in task_list:
+        print(f"ID: {task['id']},Description: {task['description']},"
+             f"Status: {task["compeletion"]},")
+
+def list_compeleted():
+    if not task_list:
+        print('to tasks found')
+    for task in task_list:
+        if task['compeleted'] == compeleted:
+            print(f"ID: {task['id']},Description: {task['description']},"
+                f"Status: {task["compeletion"]},")
+    print('task not in task list')
+
+
+def task_status():
+    task_id = input("please enter id of the task to change status")
+    for task in task_list:
+        if task['id'] == task_id:
+            task['compeleted'] = not task['completed']
+            print('task status updated')
+            return
+
+
+
+def task_priority():
+    task_id = input('enter the if of the task to which you want to set priority')
+    priority = input("please enter the priority of the task(lwo,medium,high)").lower()
+    for task in task_list:
+        if task['id'] == task_id:
+            if priority in('low','medium','high'):
+                task['priority'] = priority
+                print("task priority updated")
+            else:
+                print("wrong priority please enter either low, medium or high")
+            return
+        print('task not foun in task list check id')
+
+while True:
+    print("Welcome to the task list menue.\nto add a task press 1\nto remove a task press 2\n"
+          "to list all the tasks press 3\nto change task status press 4\nto list compeleted tasks press 5\n"
+          "to change task priority press 6\nto exit press q")
+
+    choice = input('please choose the desired operation: ')
+
+    if choice == '1':
+        add_task()
+    if choice == '2':
+        remove_task()
+    if choice == '3':
+        list_tasks()
+    if choice == '4':
+        task_status()
+    if choice == '5':
+        list_compeleted()
+    if choice == '6':
+        task_priority()
+    if choice =='q':
+        print('quitting')
+        break
+
+
